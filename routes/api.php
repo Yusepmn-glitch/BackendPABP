327224351d6613c187521dc07523dd4270d09da2
Yusepmn-glitch <247006111186@student.unsil.ac.id>
2026-05-01 02:16:50 +0700

    Fix: Route login not defined error

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\Api\FacilityController;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
